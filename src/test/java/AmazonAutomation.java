9e3a50480e1af74b33509cd1ed6401a3677d3bb0
Okuducu4609 <okuducu4609@gmail.com>
2021-12-31 23:30:43 +0100

    Step-4 project finished

--- src/test/java/AmazonAutomation.java
+++ src/test/java/AmazonAutomation.java
@@ -1,3 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
+
 public class AmazonAutomation {
      /*Steps to Automate:
     1. Open this url  http://automationpractice.com/index.php
@@ -7,4 +16,25 @@ public class AmazonAutomation {
     5. Enter your Personal Information, Address and Contact info. ismail
     6. Click on Register button. muhammad
     7. Validate that user is created.*/ // orhan
+
+
+
+
+
+
+    @Test
+
+    public void AccountButton() throws InterruptedException {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://automationpractice.com/index.php");
+
+    //4-Step
+    Thread.sleep(2000);
+    WebElement accountButton = driver.findElement(By.xpath("(//button/span)[2]"));
+    accountButton.click();
+
+
+}
+
+
 }
