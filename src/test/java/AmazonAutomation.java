e9feb20cf697577bb5b221412dfce8d3852ff832
busranurozturk <busrnurozturk@gmail.com>
2021-12-31 14:53:35 +0100

    new project started by Büsra

---
+++ src/test/java/AmazonAutomation.java
@@ -0,0 +1,10 @@
+public class AmazonAutomation {
+     /*Steps to Automate:
+    1. Open this url  http://automationpractice.com/index.php
+    2. Click on sign in link.
+    3. Enter your email address in 'Create and account' section. hilal
+    4. Click on Create an Account button. seref
+    5. Enter your Personal Information, Address and Contact info. ismail
+    6. Click on Register button. muhammad
+    7. Validate that user is created.*/ // orhan
+}
