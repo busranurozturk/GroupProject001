be4a5771929c998c45480f5f3ff9256e918ed04a
busranurozturk <busrnurozturk@gmail.com>
2021-12-31 15:06:19 +0100

    utilities created signIn button clicked

--- src/test/java/AmazonAutomation.java
+++ src/test/java/AmazonAutomation.java
@@ -1,3 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
 public class AmazonAutomation {
      /*Steps to Automate:
     1. Open this url  http://automationpractice.com/index.php
@@ -7,4 +13,11 @@ public class AmazonAutomation {
     5. Enter your Personal Information, Address and Contact info. ismail
     6. Click on Register button. muhammad
     7. Validate that user is created.*/ // orhan
+     @Test
+     public void amazon() {
+         WebDriver driver = WebDriverFactory.getDriver("chrome");
+         driver.get("http://automationpractice.com/index.php");
+         WebElement signInButton = driver.findElement(By.xpath("//*[@*='login']"));
+         signInButton.click();
+     }
 }
